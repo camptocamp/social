cdaddbc6867f44fcd89c79defbd9680863dfbe5a
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-10-26 18:26:09 +0200

    [11.0] Add module mail_inline_styles

---
+++ mail_inline_styles/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mail Inline Styles",
+    "version": "11.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://github.com/OCA/social",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [
+            'premailer'
+        ],
+    },
+    "depends": [
+        "email_template_qweb",
+    ],
+    "data": [
+    ],
+    "demo": [
+        "demo/demo_template.xml",
+        "demo/demo_mail_template.xml",
+    ],
+}
---
+++ mail_inline_styles/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Convert styles to inline styles and make them play nice with emails.
