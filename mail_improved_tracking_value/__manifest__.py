197ca7e68898361f5923bfd8ecda8c77736fa639
duyanh <duyanhitspkt@gmail.com>
2022-09-27 10:25:39 +0700

    [15.0][MIG] mail_improved_tracking_value: Migration to 15.0

--- mail_improved_tracking_value/__manifest__.py
+++ mail_improved_tracking_value/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2018 Camptocamp SA
+# Copyright 2018-2022 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Improved tracking value change",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "summary": "Improves tracking changed values for certain type of fields."
     "Adds a user-friendly view to consult them.",
     "author": "Camptocamp, Odoo Community Association (OCA)",
