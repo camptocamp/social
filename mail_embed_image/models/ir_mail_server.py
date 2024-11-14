6dc1479590fba02e04e38aa908dcda3148864136
Stephane Mangin <stephane.mangin@camptocamp.com>
2024-11-14 18:33:11 +0100

    [16.0][IMP] mail_embed_image: improve embedding type:  - CIDs are not working in some email managers (gmail and office365)    To allow this behavior we need to change the content type to    multipart/related  - A new option to embed images is to include the base64 content inside    the src.  - Then to select these options, a selected field has been added to company

--- mail_embed_image/models/ir_mail_server.py
+++ mail_embed_image/models/ir_mail_server.py
@@ -1,3 +1,7 @@
+# Copyright 2019 Therp BV <https://therp.nl>
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 import logging
 import uuid
 from base64 import b64encode
@@ -32,8 +36,9 @@ class IrMailServer(models.Model):
         body_alternative=None,
         subtype_alternative="plain",
     ):
+        image_embedding_method = self.env.company.image_embedding_method
         fileparts = None
-        if subtype == "html":
+        if subtype == "html" and image_embedding_method != "none":
             body, fileparts = self._build_email_replace_img_src(body)
         result = super(IrMailServer, self).build_email(
             email_from=email_from,
@@ -55,6 +60,10 @@ class IrMailServer(models.Model):
         if fileparts:
             for fpart in fileparts:
                 result.attach(fpart)
+            # Multipart method MUST be multipart/related for CIDs embedding
+            # Gmail and Office won't process the images otherwise
+            if image_embedding_method == "cid":
+                result.set_type("multipart/related")
         return result
 
     def _build_email_replace_img_src(self, html_body):
@@ -62,23 +71,34 @@ class IrMailServer(models.Model):
         if not html_body:
             return html_body
 
+        base_url = self.env["ir.config_parameter"].get_param("web.base.url")
+        image_embedding_method = self.env.company.image_embedding_method
         root = fromstring(html_body)
-        images = root.xpath("//img")
         fileparts = []
-        for img in images:
-            src = img.get("src")
-            if src and not src.startswith("data:") and not src.startswith("base64:"):
-                try:
-                    response = requests.get(src, timeout=10)
-                    _logger.debug("Fetching image from %s", src)
-                    if response.status_code == 200:
+        # Limit results to only internal resources to avoid malicious external
+        # image injections
+        for img in root.xpath(
+            ".//img[starts-with(@src, '%s')]"
+            "| .//img[starts-with(@src, '/web/image')]" % (base_url)
+        ):
+            image_path = img.get("src")
+            try:
+                response = requests.get(image_path, timeout=10)
+                _logger.debug("Fetching image from %s", image_path)
+                if response.status_code == 200:
+                    image_content = response.content
+                    filepart = MIMEImage(image_content)
+                    if image_embedding_method == "data":
+                        raw_content = filepart.get_payload(decode=True)
+                        base_64_content = b64encode(raw_content).decode("utf-8")
+                        mimetype = filepart.get_content_type()
+                        img.set("src", f"data:{mimetype};base64,{base_64_content}")
+                    elif image_embedding_method == "cid":
                         cid = uuid.uuid4().hex
                         # convert cid to rfc2047 encoding
                         filename_encoded = "=?utf-8?b?%s?=" % b64encode(
                             cid.encode("utf-8")
                         ).decode("utf-8")
-                        image_content = response.content
-                        filepart = MIMEImage(image_content)
                         filepart.add_header("Content-ID", f"<{cid}>")
                         filepart.add_header(
                             "Content-Disposition",
@@ -87,6 +107,12 @@ class IrMailServer(models.Model):
                         )
                         img.set("src", f"cid:{cid}")
                         fileparts.append(filepart)
-                except Exception as e:
-                    _logger.warning("Could not get %s: %s", img.get("src"), str(e))
+                else:
+                    _logger.warning(
+                        "Could not get %s: HTTP status code %s",
+                        img.get("src"),
+                        response.status_code,
+                    )
+            except Exception as e:
+                _logger.warning("Could not get %s: %s", img.get("src"), str(e))
         return tostring(root, encoding="unicode"), fileparts
