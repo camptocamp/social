2f822f77ce496176d5fbe33ee3360b2dd7a1e735
Luca-Policastro <luca.policastro@camptocamp.com>
2025-05-28 11:08:02 +0200

    [MIG] outgoing_email_by_model: Migration to 17.0

--- outgoing_email_by_model/tests/test_mailserver_by_model.py
+++ outgoing_email_by_model/tests/test_mailserver_by_model.py
@@ -3,10 +3,12 @@
 
 from odoo_test_helper import FakeModelLoader
 
-from odoo.tests.common import Form, TransactionCase
+from odoo.tests import Form
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestMailserverByModel(TransactionCase):
+class TestMailserverByModel(BaseCommon):
     at_install = False
     post_install = True
 
@@ -72,7 +74,7 @@ class TestMailserverByModel(TransactionCase):
         composer = Form(
             self.env["mail.compose.message"].with_context(
                 default_model=record._name,
-                default_res_id=record.id,
+                default_res_ids=record.ids,
                 default_use_template=True,
                 default_template_id=self.mail_template.id,
                 default_composition_mode="comment",
